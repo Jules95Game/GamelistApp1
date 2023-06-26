1ece13ecb3d6a541db85bb54e5dd64d4df906477
Jules95Game <romp.julian1@gmail.com>
2023-06-26 14:41:33 +0200

    Add project files.

---
+++ GamelistApp1.TestConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GamelistApp1.TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
