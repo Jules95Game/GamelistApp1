59f8bab71acf4abb0d7a42e663c3009985916b9c
Jules95Game <romp.julian1@gmail.com>
2023-06-27 17:20:22 +0200

    Added initial migration

---
+++ GamelistApp1.DataAccess/Migrations/20230627151931_InitializeAndSeed.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GamelistApp1.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeAndSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Developer = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Platform = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Games",
+                columns: new[] { "Id", "Description", "Developer", "Platform", "Publisher", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Wahoo!", "Nintendo EPD", "Nintendo Switch", "Nintendo", "Super Mario Odyssey" },
+                    { 2, "Yahaha!", "Nintendo EPD", "Nintendo Switch", "Nintendo", "The Legend of Zelda: Breath of the Wild" },
+                    { 3, "Haiya!", "HAL Laboratory", "Nintendo Switch", "Nintendo", "Kirby's Return to Dream Land Deluxe" },
+                    { 4, "Hey! Listen!", "Nintendo EAD", "Nintendo 64", "Nintendo", "The Legend of Zelda: Ocarina of Time" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
