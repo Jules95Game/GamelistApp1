9b24f3e2eac6cd961212e2fb573d7451770893e8
Jules95Game <romp.julian1@gmail.com>
2023-06-28 17:07:08 +0200

    Added Repository<TEntity> class

---
+++ GamelistApp1.DataAccess/Repository.cs
@@ -0,0 +1,31 @@
+﻿using GamelistApp1.Domain.Interfaces;
+
+namespace GamelistApp1.DataAccess;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : IEntity
+{
+    public IQueryable<TEntity> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntity Create(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntity Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
