18e7f7f379b99a2fe83ab7469ebb6bf390ece9e5
Jules95Game <romp.julian1@gmail.com>
2023-06-29 10:15:49 +0200

    Added GameService class

---
+++ GamelistApp1.Business/Services/GameService.cs
@@ -0,0 +1,42 @@
+﻿using GamelistApp1.Domain.Interfaces;
+using GamelistApp1.Domain.Models;
+
+namespace GamelistApp1.Business.Services;
+
+public class GameService : IService<Game>
+{
+    public List<Game> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Game> GetTop100()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Game> SearchByQuery(string query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Game GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Game Create(Game entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Game Update(Game entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
