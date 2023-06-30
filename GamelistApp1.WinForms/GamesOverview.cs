51a64f74ff025e4d355dac3d1f79698efa9fb2d9
Jules95Game <romp.julian1@gmail.com>
2023-06-30 17:39:35 +0200

    Configured IHost field

--- GamelistApp1.WinForms/GamesOverview.cs
+++ GamelistApp1.WinForms/GamesOverview.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using GamelistApp1.Business.Services;
+using GamelistApp1.DataAccess;
+using GamelistApp1.Domain.Interfaces;
+using GamelistApp1.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +20,21 @@ namespace GamelistApp1.WinForms
 {
     public partial class GamesOverview : Form
     {
+        private readonly IHost _host;
+
         public GamesOverview()
         {
             InitializeComponent();
+            
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddDbContext<DataContext>(options =>
+                        options.UseSqlServer(context.Configuration.GetConnectionString("Default")));
+                    services.AddScoped<IRepository<Game>, Repository<Game>>();
+                    services.AddScoped<IService<Game>, GameService>();
+                })
+                .Build();
         }
     }
 }
