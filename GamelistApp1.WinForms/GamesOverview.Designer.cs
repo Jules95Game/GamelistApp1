d5962f3ba4f1a2d88a5951bf56e79b4bc0578292
Jules95Game <romp.julian1@gmail.com>
2023-06-30 16:03:50 +0200

    Added Searchbox, Listbox and Add/Remove buttons

--- GamelistApp1.WinForms/GamesOverview.Designer.cs
+++ GamelistApp1.WinForms/GamesOverview.Designer.cs
@@ -3,12 +3,12 @@
     partial class GamesOverview
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,73 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            searchBox = new TextBox();
+            listBox1 = new ListBox();
+            btnAdd = new Button();
+            btnRemove = new Button();
+            SuspendLayout();
+            // 
+            // searchBox
+            // 
+            searchBox.Location = new Point(12, 12);
+            searchBox.Name = "searchBox";
+            searchBox.Size = new Size(200, 23);
+            searchBox.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 41);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(200, 394);
+            listBox1.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(12, 451);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(97, 23);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new Point(115, 451);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(97, 23);
+            btnRemove.TabIndex = 3;
+            btnRemove.Text = "Remove";
+            btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // GamesOverview
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(859, 486);
+            Controls.Add(btnRemove);
+            Controls.Add(btnAdd);
+            Controls.Add(listBox1);
+            Controls.Add(searchBox);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "GamesOverview";
+            Text = "Game Collection";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox searchBox;
+        private ListBox listBox1;
+        private Button btnAdd;
+        private Button btnRemove;
     }
 }
