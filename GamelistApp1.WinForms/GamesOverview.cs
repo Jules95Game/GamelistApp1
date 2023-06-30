115be6b522c9dabdf4f1f5e4f634ca59fef84e2b
Jules95Game <romp.julian1@gmail.com>
2023-06-30 15:19:50 +0200

    Added WinForms project

---
+++ GamelistApp1.WinForms/GamesOverview.cs
@@ -0,0 +1,10 @@
+namespace GamelistApp1.WinForms
+{
+    public partial class GamesOverview : Form
+    {
+        public GamesOverview()
+        {
+            InitializeComponent();
+        }
+    }
+}
